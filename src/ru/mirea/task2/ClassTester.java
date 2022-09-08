9f2d22cf132912412fef6cd785489660fcb826d4
Nadezhda Erdneeva <erdneeva0363@gmail.com>
2022-09-08 09:11:49 +0300

    task2(tester)

---
+++ src/ru/mirea/task2/ClassTester.java
@@ -0,0 +1,8 @@
+package ru.mirea.task2;
+
+public class ClassTester {
+    public static void main(String[] args) {
+        Shape shape = new Shape("circle", 10, 4, 6);
+        System.out.println(shape);
+    }
+}
